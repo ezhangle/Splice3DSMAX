914b6ba0e3d9e690796253fa0ccd974de50bd795
phtaylor <phil.taylor@fabricengine.com>
2014-08-27 15:00:49 -0500

    Max launcher working, and the KL Editor working.

--- 3dsmax2014.bat
+++ 3dsmax2014.bat
@@ -4,12 +4,14 @@
 
 
 set CURRDIR=%~dp0
+
 rem Ensure that the Splice plugin will be able to find the extensions 
 set FABRIC_EXTS_PATH=%FABRIC_EXTS_PATH%;%CURRDIR%..\..\Exts
 
-rem Ensure the Splice API folder and the Scintilla folder are included in the paths. 
-set PATH=%PATH%;%CURRDIR%\ScintillaNET
+rem Save the Scintilla folder to an environment variable that will be accessed in MaxScript
+set SCINTILLANETDIR=%CURRDIR%ScintillaNET\
 
+rem Launch Max
 call "D:\Program Files\Autodesk\3ds Max 2014\3dsmax.exe"
 
 echo on

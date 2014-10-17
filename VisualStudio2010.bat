a80bbed0ad2c469f0fa18262181f29d1e9c0db0c
Stephen Taylor <stephen.taylor.dev@gmail.com>
2014-10-17 11:13:19 -0400

    Tweaked VS script to be slightly more user friendly.

--- VisualStudio2010.bat
+++ VisualStudio2010.bat
@@ -1,22 +1,27 @@
-@echo on
+@echo OFF
 
 set REPODIR=%~dp0
 set SLN=%REPODIR%/FabricSplice3dsmax.sln
 set SGREPODIR=%REPODIR%\..\..\..
-set FABRIC_DIR=%REPODIR%\..\..\..\stage\Windows\x86_64\Release
-set BOOST_DIR=%SGREPODIR%\ThirdParty\PreBuilt\Windows\x86_64\Release\boost\1.55.0\lib
 
-set DISTDIR=%FABRIC_DIR%\SpliceIntegrations\FabricSplice3dsmax2014
+IF NOT DEFINED FABRIC_DIR (
+  SET FABRIC_DIR=%REPODIR%\..\..\..\stage\Windows\x86_64\Release
+  ECHO Setting Fabric at %FABRIC_DIR%
+  SET FABRIC_EXTS_PATH=%FABRIC_EXTS_PATH%;%FABRIC_DIR%\Exts
+)
+else ECHO Fabric found at %FABRIC_PATH%
 
 echo Allow the system to load FabricCore.1.13.dll
-set PATH=%FABRIC_LIB_DIR%;%PATH%
+SET PATH=%FABRIC_DIR%/lib;%PATH%
 
-set FABRIC_EXTS_PATH=%FABRIC_EXTS_PATH%;%FABRIC_DIR%\Exts
+IF NOT DEFINED BOOST_DIR SET BOOST_DIR=%SGREPODIR%\ThirdParty\PreBuilt\Windows\x86_64\Release\boost\1.55.0\lib
+
+set DISTDIR=%FABRIC_DIR%\SpliceIntegrations\FabricSplice3dsmax2014
 set SCINTILLANETDIR=%DISTDIR%ScintillaNET\
 
-call %SGREPODIR%\Environment.bat
+rem call %SGREPODIR%\Environment.bat
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" %SLN%
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "%SLN%"
 
 pause.
 

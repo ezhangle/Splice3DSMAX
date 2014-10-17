e38f8206a9dcf6947cbb1d341d063849cf548c2f
Stephen Taylor <stephen.taylor.dev@gmail.com>
2014-10-17 11:14:49 -0400

    Missed from the last commit - build script tweak

--- VisualStudio2010.bat
+++ VisualStudio2010.bat
@@ -2,7 +2,6 @@
 
 set REPODIR=%~dp0
 set SLN=%REPODIR%/FabricSplice3dsmax.sln
-set SGREPODIR=%REPODIR%\..\..\..
 
 IF NOT DEFINED FABRIC_DIR (
   SET FABRIC_DIR=%REPODIR%\..\..\..\stage\Windows\x86_64\Release
@@ -14,14 +13,16 @@ else ECHO Fabric found at %FABRIC_PATH%
 echo Allow the system to load FabricCore.1.13.dll
 SET PATH=%FABRIC_DIR%/lib;%PATH%
 
-IF NOT DEFINED BOOST_DIR SET BOOST_DIR=%SGREPODIR%\ThirdParty\PreBuilt\Windows\x86_64\Release\boost\1.55.0\lib
+IF NOT DEFINED BOOST_DIR ( 
+  set SGREPODIR=%REPODIR%\..\..\..
+  SET BOOST_DIR=%SGREPODIR%\ThirdParty\PreBuilt\Windows\x86_64\Release\boost\1.55.0\lib
+)
 
 set DISTDIR=%FABRIC_DIR%\SpliceIntegrations\FabricSplice3dsmax2014
 set SCINTILLANETDIR=%DISTDIR%ScintillaNET\
 
-rem call %SGREPODIR%\Environment.bat
-
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "%SLN%"
+echo Starting VS Launcher
+call "C:\Program Files (x86)\Common Files\microsoft shared\MSEnv\VSLauncher.exe" "%SLN%"
 
 pause.
 

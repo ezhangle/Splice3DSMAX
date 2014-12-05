c2c477fff30ee2a704f5cda5ac59160ddf825303
phtaylor <phil.taylor@fabricengine.com>
2014-12-05 09:59:25 -0500

    Added copySamples batch file. build system is mostly working.

--- build-all.sh
+++ build-all.sh
@@ -1,2 +1,3 @@
-#cmd //c build2014.bat
+cmd //c build2014.bat
 cmd //c build2015.bat
+cmd //c copySamples.bat
---
+++ copySamples.bat
@@ -0,0 +1,2 @@
+
+xcopy "%FABRIC_SCENE_GRAPH_DIR%\Python\Apps\Resources\Splice\3dsmax" "%FABRIC_DIR%\Samples\3dsmax" /E /I /Y

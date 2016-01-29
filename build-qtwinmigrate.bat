734ee8f0447bdf884eb01564ee38849772786528
Stephen Taylor <stephen.taylor.dev@gmail.com>
2016-01-29 06:08:38 -0500

    Creating a build-file that handles all possible issues with building QTWinMigrate

--- build-qtwinmigrate.bat
+++ build-qtwinmigrate.bat
@@ -1,6 +1,62 @@
-makelocal
+setlocal  enableextensions enabledelayedexpansion
+@echo off
+IF NOT DEFINED QT_DIR (
+    ECHO ERROR: QT_DIR not set.  Please ensure QT_DIR is set or nmake is on the PATH
+    GOTO ERROR_EXIT
+)
+
+SET PATH=%PATH%;%QT_DIR%\bin
+WHERE qmake > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO ERROR: qmake not found.  Please ensure QT_DIR is set or nmake is on the PATH
+    GOTO ERROR_EXIT
+)
+ECHO Found qmake...
+
+WHERE nmake > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO ERROR: nmake not found.  Please run this script from a Visual Studio Command Prompt
+    GOTO ERROR_EXIT
+)
+for /f "tokens=*" %%i in ('WHERE nmake') do set NMAKE_PATH=%%i
+If "%NMAKE_PATH%"=="%NMAKE_PATH:amd64=%" (
+    echo ERROR: Win32 nmake detected.  We only support x64 versions, please start the right version of VS Command Prompt 
+    GOTO ERROR_EXIT
+)
+ECHO Found nmake...
+
+REM Enforce VS 2013 (at least for now)
+SET QMAKESPEC=%QT_DIR%\mkspecs\win32-msvc2013
+
+REM --------------------------------------------------
+REM Get around Qt's rediculous hard-coded binary paths
+REM --------------------------------------------------
+
+SET _QT_CONF_F=%QT_DIR%\bin\qt.conf
+SET QT_DIR_FWDSLASH=%QT_DIR:\=/%
+IF NOT EXIST %_QT_CONF_F% (
+    echo  -- Generating new qt.conf file @ %_QT_CONF_F% -- 
+
+    @echo [Paths] >> %_QT_CONF_F%
+    @echo Prefix = %QT_DIR_FWDSLASH% >> %_QT_CONF_F%
+)
+
 cd "%~dp0qt-solutions\qtwinmigrate"
 configure -library
+nmake distclean
 qmake
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO ERROR: qmake exitted with non-0 status
+  goto ERROR_EXIT
+)
 nmake
-endlocal
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO ERROR: nmake exitted with non-0 status
+  goto ERROR_EXIT
+)
+
+echo -- Build Successful
+:ERROR_EXIT
+pause
+:EOF
+REM endlocal

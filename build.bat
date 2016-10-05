30c7185250e833f013ebefd57ca17f1b8e58170b
Borja Morales <borja.morales@fabricengine.com>
2016-10-05 16:16:00 +0200

    Changed the vcvarsall target

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 msbuild /m:%1 /p:Configuration="Release %2" /p:Platform=%3 %4
 set batdir=%~dp0
 set FabricStageDir=%batdir%stage/Fabric3dsmax%2
